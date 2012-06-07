821021c8da5a1838d0dbb39bb34c673264134bc9
Dimitris Baltas <dimitris@codemax.gr>
2012-06-07 12:24:09 +0300

    search, stations and play buttons visual

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,17 +1,130 @@
 Ext.define("pandora.view.Main", {
     extend: 'Ext.Container',
-    requires: [
-    ],
     config: {
-		items: [{
-			xtype: 'toolbar',
-			docked: 'top'
-		}, {
-			html: 'Left List',
-			docked: 'left',
-			width: 250
-		}, {
-			html: 'Center Area'
-		}]
+        items: [{
+            docked: 'top',
+            xtype: 'toolbar',
+            height: 90,
+            items: [
+                {
+                    xtype: 'selectfield',
+                    placeHolder: 'Search station...',
+                    width: 180
+                },
+                {
+                    xtype: 'container',
+                    flex: 1,
+                    layout: {
+                        type: 'vbox',
+                        align: 'center'
+                    },
+                    items: [
+                        {
+                            xtype: 'container',
+                            defaults: {
+                                xtype: 'button',
+                                ui: 'plain',
+                                iconMask: true
+                            },
+                            layout: 'hbox',
+                            items: [{
+                                iconCls: 'star',
+                                action: 'vote-up'
+                            }, {
+                                iconCls: 'flag',
+                                action: 'vote-down'
+                            }, {
+                                iconCls: 'pause',
+                                action: 'pause'
+                            }, {
+                                iconCls: 'fforward',
+                                action: 'skip'
+                            }]
+                        },
+                        {
+                            xtype: 'container',
+                            layout: {
+                                type: 'hbox',
+                                align: 'center'
+                            },
+                            items: [{
+                                xtype: 'component',
+                                html: '2:00'
+                            }, {
+                                xtype: 'slider',
+                                value: 50,
+                                width: 200,
+                                minValue: 0,
+                                maxValue: 100
+                            }, {
+                                xtype: 'component',
+                                html: '4:00'
+                            }]
+                        }
+                    ]
+                },
+                {
+                    xtype: 'component',
+                    cls: 'app-logo',
+                    html: 'Sencha Radio'
+                }
+            ]
+        }, {
+            width: 250,
+            docked: 'left',
+            xtype: 'list',
+            grouped: true,
+            store: {
+                data: [
+                    {name: 'Black Star'},
+                    {name: 'Led Zeppelin'}
+                ],
+                grouper: function(record) {
+                    return record.get('name')[0];
+                }
+            },
+            itemTpl: '{name}',
+            items: [
+                {
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Stations',
+                    ui: 'light'
+                },
+                {
+                    docked: 'bottom',
+                    xtype: 'toolbar',
+                    ui: 'light',
+                    items: [
+                        {
+                            xtype: 'button',
+                            action: 'settings',
+                            iconMask: true,
+                            iconCls: 'settings',
+                            ui: 'plain'
+                        },
+                        {
+                            xtype: 'segmentedbutton',
+                            layout: {
+                                type: 'hbox',
+                                align: 'middle'
+                            },
+                            items: [
+                                {
+                                    text: 'By Date',
+                                    action: 'filter-date'
+                                },
+                                {
+                                    text: 'ABC',
+                                    action: 'filter-name'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }, {
+            html: 'Center area takes up the rest of the space'
+        }]
     }
 });
